2e8ada6c3182c0fe92da77475a8eca16229f01d4
geadlydrim <keanuagustin.tech@gmail.com>
2025-09-29 07:12:44 +0800

    added functions for ux and changed function names to snake case

--- toDoApp.py
+++ toDoApp.py
@@ -4,21 +4,25 @@ import os
 
 tasks=[]
 
-def clearTerminal():
+def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
     
-def addTask(new_task):
+def wait_for_key():
+    input("Press Enter to continue...")
+    clear_terminal()
+    
+def add_task(new_task):
     tasks.append(new_task)
     print("Task added successfully!")
 
-def showTasks():
+def show_tasks():
     if len(tasks) == 0:
         print("No tasks added yet.")
     else:
         for i in range (len(tasks)):
             print(i+1, ".", tasks[i])
 
-def removeTask(task_number):
+def remove_task(task_number):
     tasks.pop(task_number) 
     print("Task removed successfully!!")
 
@@ -30,17 +34,18 @@ def main():
         print("[4] Exit")
         
         choice = input("Enter choice: ")
-        clearTerminal()
+        clear_terminal()
         
         match choice:
             case "1":
                 new_task = input("Enter a task: ")
-                addTask(new_task)
+                add_task(new_task)
             case "2":
-                showTasks()
+                show_tasks()
+                wait_for_key()
             case "3":
                 task_number = int(input("Enter a task number to be removed: "))
-                removeTask(task_number)
+                remove_task(task_number)
             case "4":
                 break
             case _:
